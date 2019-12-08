72019c8a5a6d62b5f86debb6b9b485f412faabcf
hd-prajwalgowda <hdprajwalgowda@gmail.com>
2019-12-08 21:49:02 +0530

    its final

--- frontend/src/Components/CreateRooms.js
+++ frontend/src/Components/CreateRooms.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import svgLogin from "./Login/undraw_online_chat_d7ek.svg";
-import { IP } from "./config";
+import { IP, KEY } from "./config";
 import { Form, Button, notification } from "antd";
 
 class CreateRooms extends Component {
